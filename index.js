d386c0a363a02f276527f10dccaf0b3b11447a50
Robert Kowalski <rgkmg25@gmail.com>
2022-03-24 23:44:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const romanValues = new Map([
-  ["I", 1],
-  ["V", 5],
-  ["X", 10],
-  ["L", 50],
-  ["C", 100],
+  ["M", 1000],
   ["D", 500],
-  ["M", 1000]
+  ["C", 100],
+  ["L", 50],
+  ["X", 10],
+  ["V", 5],
+  ["I", 1]
 ]);
 
 export default (romanNumerals, reduce = false) => {
