5ae47c2f566d5dd9bf0a9040d499fa1f00c303fd
Robert Kowalski <rgkmg25@gmail.com>
2022-10-24 23:59:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ export default (romanNumerals, reduce = false) => {
 
   let optimize = "";
 
-  for (let i = 0, sum = 0, amount = total / romanArray[i][1]; i < romanArray.length; i++, amount = (total - sum) / romanArray[i][1]) {
+  for (
+    let i = romanArray.findIndex(element => (element / 2) < total), sum = 0, amount = total / romanArray[i][1];
+    i < romanArray.length;
+    i++, amount = (total - sum) / romanArray[i][1]
+  ) {
     // Stop when no amount is remaining.
     if (amount <= 0) break;
 
@@ -47,7 +51,7 @@ export default (romanNumerals, reduce = false) => {
 
     // This loop is for reduces characters, effecient lookup to the roman numeral and next 2 lower values.
     for (let j = i + 1, len = Math.max(j + 2, romanArray.length); j < len; j++) {      
-      // If a roman numeral index is even, you can not minus a negative index roman numeral, for example VX should always be just V.
+      // If a roman numeral index is even, you can not minus a odd index roman numeral, for example VX should always be just V.
       // Uses bitwise operator & as no roman numeral can be bigger then a 32bit integer and its faster.
       if (!(i & 1) && j & 1) continue;
 
