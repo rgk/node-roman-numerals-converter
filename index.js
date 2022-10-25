befcb47bcb1f0143a23c35cbaeb099d4747c4249
Robert Kowalski <rgkmg25@gmail.com>
2022-10-25 02:47:51 -0400

    Min should be the right answer.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ export default (romanNumerals, reduce = false) => {
 
   let optimize = "";
 
-  for (let i = 0, sum = 0, amount = 0; i < romanArray.length; i++) {
+  for (let i = romanArray.findIndex(element => element[1] / 2 < total), sum = 0, amount = 0; i < romanArray.length; i++) {
     // Stop when no amount is remaining.
     if (amount = (total - sum) / romanArray[i][1] <= 0) break;
 
@@ -46,7 +46,7 @@ export default (romanNumerals, reduce = false) => {
     optimize += romanArray[i][0].repeat(amount);
 
     // This loop is for reduces characters, effecient lookup to the roman numeral and next 2 lower values.
-    for (let j = i + 1, len = Math.min(j + 2, romanArray.length); j < len; j++) {      
+    for (let j = i + 1, len = Math.min(j + 3, romanArray.length); j < len; j++) {      
       // If a roman numeral index is even, you can not minus a odd index roman numeral, for example VX should always be just V.
       // Uses bitwise operator & as no roman numeral can be bigger then a 32bit integer and its faster.
       if (!(i & 1) && j & 1) continue;
