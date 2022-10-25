0a415f72b6a93e716497d6e089534e12adca3a82
Robert Kowalski <rgkmg25@gmail.com>
2022-10-25 04:24:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ export default (romanNumerals, reduce = false) => {
 
   let optimize = "";
 
-  for (let i = romanArray.findIndex(element => element[1] / 2 < total), sum = 0, amount = 0; 0 < i < romanArray.length; i++) {
+  for (let i = romanArray.findIndex(element => element[1] / 2 < total), sum = 0, amount = 0, len = ~i && romanArray.length; i < len; i++) {
     // Stop when no amount is remaining.
     if (i === -1 || !(amount = (total - sum) / romanArray[i][1])) break;
 
