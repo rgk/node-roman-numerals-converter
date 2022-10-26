b6e177c79c6fa7a199fcf93bab98a5b9f01c6128
Robert Kowalski <rgkmg25@gmail.com>
2022-10-26 06:38:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,16 +45,16 @@ export default (romanNumerals, reduce = false) => {
     sum += romanArray[i][1] * amount;
     optimize += romanArray[i][0].repeat(amount);
 
-    // This loop is for reduces characters, effecient lookup to the roman numeral and next 2 lower values.
-    for (let j = i + 1, len = Math.min(j + 3, romanArray.length); j < len; j++) {      
+    // This loop is for reducing characters, effecient lookup to the roman numeral and next 2 lower values.
+    for (let j = i + 1, value, len = Math.min(j + 3, romanArray.length); j < len; j++) {      
       // If a roman numeral index is even, you can not minus a odd index roman numeral, for example VX should always be just V.
       // Uses bitwise operator & as no roman numeral can be bigger then a 32bit integer and its faster.
       if (!(i & 1) && j & 1) continue;
 
-      const newValue = romanArray[i][1] - romanArray[j][1];
+      value = romanArray[i][1] - romanArray[j][1];
       // If the total value is larger then it should be, don't.
-      if (sum + newValue > total) continue;
-      sum += newValue;
+      if (sum + value > total) continue;
+      sum += value;
       optimize += romanArray[j][0] + romanArray[i][0];
     }
   }
