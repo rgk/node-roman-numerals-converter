1e90ab6b1d5e885cd2878d215febd80e0efa8339
Robert Kowalski <rgkmg25@gmail.com>
2022-11-02 22:32:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const values = {
 };
 
 // Produces an array for a more effecient loop.
-const romanArray = Object.entries(values);
+const list = Object.entries(values);
 
 export default (romanNumerals, reduce = false) => {
   let total = 0;
@@ -33,23 +33,23 @@ export default (romanNumerals, reduce = false) => {
 
   let optimize = "";
 
-  for (let i = romanArray.findIndex(element => element[1] / 2 < total), sum = 0, amount = 0, len = (~i) ? romanArray.length : i; i < len; i++) {
+  for (let i = list.findIndex(element => element[1] / 2 < total), sum = 0, amount = 0, len = (~i) ? list.length : i; i < len; i++) {
     // Stop when no amount is remaining.
-    if (!(amount = (total - sum) / romanArray[i][1])) break;
+    if (!(amount = (total - sum) / list[i][1])) break;
 
     // Only allow the roman numeral character to be used 3 times in a row, no float needed.
-    optimize += romanArray[i][0].repeat(amount = Math.min(Math.trunc(amount), 3));
-    sum += romanArray[i][1] * amount;
+    optimize += list[i][0].repeat(amount = Math.min(Math.trunc(amount), 3));
+    sum += list[i][1] * amount;
 
     // This loop is for reducing characters, effecient lookup to the roman numeral and next 2 lower values.
-    for (let j = i + 1, value, len = Math.min(j + 3, romanArray.length); j < len; j++) {      
+    for (let j = i + 1, value, len = Math.min(j + 3, list.length); j < len; j++) {      
       // If a roman numeral index is even, you can not minus a odd index roman numeral, for example VX should always be just V.
       // Uses bitwise operator & as no roman numeral can be bigger then a 32bit integer and its faster.
       // If the total value is larger then it should be, don't.
-      if ((!(i & 1) && j & 1) || (value = romanArray[i][1] - romanArray[j][1]) + sum > total) continue;
+      if ((!(i & 1) && j & 1) || (value = list[i][1] - list[j][1]) + sum > total) continue;
 
       sum += value;
-      optimize += romanArray[j][0] + romanArray[i][0];
+      optimize += list[j][0] + list[i][0];
     }
   }
 
